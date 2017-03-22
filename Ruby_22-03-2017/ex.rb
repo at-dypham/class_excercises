771c4bd163c8341897d33bea0f561f71b7d46e01
Trang Truong c <tnkttruong@gmail.com>
2017-03-22 11:00:48 +0700

    push ham de quy

--- Ruby_22-03-2017/ex.rb
+++ Ruby_22-03-2017/ex.rb
@@ -3,19 +3,40 @@ h = { "name" => "trang",
   :name => "tnkt",
   subhash: {
     "name" => "abc",
-    name: "abcdef"
+    name: {"name2" => "abcdef",
+      name2: "1234",
+      name22: {sublv3: "sub3",
+        sub4: "sub444"
+      }
+      },
+      sub2: 123
+
+    }
   }
-}
+# def check_hash(h)
+#   result = []
+#   h.map{|sk, sv|
+#    result << {sk => sv}
+#  }
+#  return result
+# end
+def dequy(h)
+  result = []
+  h.map{|k, v|
+    if v.is_a?(Hash)
+      result << dequy(v)
+    else
+      result << { k => v}
+    end
+  }
+  return result
+end
 result = []
 h.map{|k, v|
   if v.is_a?(Hash)
-    v.map{|sk, sv|
-     h  = { sk => sv}
-     result.push h
-   }
+   result << dequy(v)
  else
-  h  = { k => v}
-  result.push h
+  result << { k => v}
 end
 }
 puts result
