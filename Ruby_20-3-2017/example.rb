67f18a6bdd50e58520cb8f3c697471437829ebf2
Trang Truong c <tnkttruong@gmail.com>
2017-03-22 10:10:54 +0700

    creat branch 22/3/2017

--- Ruby_20-3-2017/example.rb
+++ Ruby_20-3-2017/example.rb
@@ -1,9 +1,9 @@
+#2 vis du ve block code cua view helper rails,sum
 def input
-  
   begin
     str = gets.chomp
     if str.length < 5
-        raise "min length is 5, please input again"
+      raise "min length is 5, please input again"
     end
   rescue => e
     puts "Error: #{e}"
@@ -12,8 +12,44 @@ def input
   return str
 end
 
-puts "input str1"
-str1 = input
-puts "input str2"
-str2 = input
-puts str1.length < str2.length ? "#{str1}#{str2}#{str1}" : "#{str2}#{str1}#{str2}"
+def ex1
+  puts "input str1"
+  str1 = input
+  puts "input str2"
+  str2 = input
+  puts str1.length < str2.length ? "#{str1}#{str2}#{str1}" : "#{str2}#{str1}#{str2}"
+end
+
+a= Proc.new do 
+  puts "a"
+end
+
+def test1(&a)
+  puts a
+end
+
+mut = Proc.new do |b|
+  b%3 == 0
+end
+
+def call_procs(p1, p2)
+  p1.call
+  p2.call
+end
+a = Proc.new do puts "First proc" end
+ b = Proc.new { puts "Second proc" }
+
+  lam = lambda { |x| 
+    puts x
+    return x=x*3
+    return "abc"
+  }
+  #(1..3).each(&lam)
+  def t
+    pr = Proc.new do|x,y,z|
+      return 3
+    end
+    puts pr.call
+  end
+  puts t
+  #pr.call(2)
--- Trang_Ruby_16-3/ruby.rb
+++ Trang_Ruby_16-3/ruby.rb
@@ -1,9 +1,8 @@
 require 'benchmark'
 #Monkey patching ===>>>
-#puts Benchmark.measure { "a"*1_000_000 }
 #collect, map, select,detect
 
-
+#puts Benchmark.measure { "a"*1_000_000 }
 #a=[nil]
 b = [1,2,3,4  ]
 # puts b.values_at(1)
@@ -26,21 +25,21 @@ class Object
     puts "hello"
   end
 end
-b.select{|item| 
-  puts item > 1
-}
- [{id: 10},{id: 11},{id: 9},[{id: 12},{id: 8}]].select{|us|
-  if us.class.to_s == "Array"
-    us.detect{|d|
-      d[:id]>11
-      puts d[:id]
-      }.length > 0
-    else 
-      us[:id] > 11
-       puts us[:id] 
-    end
-  }
-
+# b.select{|item| 
+#   puts item > 1
+# }
+#  [{id: 10},{id: 11},{id: 9},[{id: 12},{id: 8}]].select{|us|
+#   if us.class.to_s == "Array"
+#     us.detect{|d|
+#       d[:id]>11
+#       puts d[:id]
+#       }.length > 0
+#     else 
+#       us[:id] > 11
+#        puts us[:id] 
+#     end
+#   }
+puts b.at(3)
 
 
 
