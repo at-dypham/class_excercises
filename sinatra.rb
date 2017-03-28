d0e9ebdf10f2c68f059fae4de472989697eeed4d
Anh Nguyen <anh.nguyen2@asiantech.vn>
2017-03-28 14:05:07 +0700

    sinatra test file

---
+++ sinatra.rb
@@ -0,0 +1,29 @@
+require "sinatra"
+require "pry"
+
+# get '/hello/:name' do
+#   "Hello #{params['name']}!"
+# end
+
+# Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+get '/hello' do #string tren url
+  erb :hello #file hello.erb trong folder views
+end
+
+post '/say_something' do
+  #binding.pry
+  @name = params[:name]
+  @say_something = params[:say_something]
+  #Write file
+  File.open("info.txt", 'w') { |file| file.write(@name + ", " + @say_something) }
+  
+  #Read file
+  fileObj = File.new("info.txt", "r")
+  @message = fileObj.gets
+  # while (line = fileObj.gets)
+  #   @message = line
+  # end
+  fileObj.close
+  erb :hello
+end
