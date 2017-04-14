285f2c2e962b75c8bb9c314a3371c82d2c24d5b0
Duy Nguyen P <duy.nguyen@asiantech.vn>
2017-04-14 16:45:01 +0700

    Update rails

--- myapp/app/controllers/users_controller.rb
+++ myapp/app/controllers/users_controller.rb
@@ -1,3 +1,33 @@
 class UsersController < ApplicationController
+  def index
+    @user= User.all
+  end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    @user.save
+    redirect_to users_path
+  end
+
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+  def update
+      @user = User.find_by id: params[:id]
+      @user.update_attributes user_params
+  end
+  def destroy
+    @user = User.find_by id: params[:id]
+    @user.destroy
+  end
+  def show
+    @user = User.find_by id: params[:id]
+  end
+  def user_params
+    params.require(:user).permit :first_name, :last_name, :description
+  end
 end
